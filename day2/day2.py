04a3b3d220b856c3b9783b984da75f7430be3e13
Justin Roberts <justinrroberts@gmail.com>
2020-12-01 18:03:59 -0600

    day2 setup

---
+++ day2/day2.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+
+INPUT = "day2_input.txt"
+INPUT = "day2_example.txt"
+
+# get input
+f = [l.strip() for l in open(INPUT).readlines()]
+
+def Part1():
+    return 
+
+def Part2():
+    return 
+
+# execute part 1
+print("Part 1: ", Part1())
+# execute part 2
+print("Part 2: ", Part2())
